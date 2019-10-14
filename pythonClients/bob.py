15f04345dad8a95bbd0b269cf942796898497ab9
HackyZach <zach.owen50@gmail.com>
2019-10-13 18:56:07 -0700

    Modified blog files.

--- pythonClients/bob.py
+++ pythonClients/bob.py
@@ -6,10 +6,8 @@ with xmlrpc.client.ServerProxy("http://localhost:5000") as proxy:
     tuples = [
       ["bob","distsys","I am studying chapter 2"],
       ["bob","distsys","The rinda exmaple's not that simple"],
-      ["bob","distsys",1234],
-      ["bob","distsys",678],
-      ["bob","distsys",4784],
-      ["bob","distsys","ehhh...."]
+      ["bob","distsys","ehhh...."],
+      ["bob","distsys","It's only a distributed systems model..."],
     ]
 
     print("Tuples Ready...")
@@ -18,7 +16,5 @@ with xmlrpc.client.ServerProxy("http://localhost:5000") as proxy:
     proxy._out(tuples[1])
     proxy._out(tuples[2])
     proxy._out(tuples[3])
-    proxy._out(tuples[4])
-    proxy._out(tuples[5])
 
     print("Tuples Sent...")
--- pythonClients/chuck.py
+++ pythonClients/chuck.py
@@ -7,9 +7,9 @@ proxy = xmlrpc.client.ServerProxy("http://localhost:5000")
 
 print("Waiting on tuples...")
 
-res = proxy._in(["bob", { 'class': 'String' }, { 'class': 'String' }])
+res = proxy._in([{ 'class': 'String'}, "distsys", { 'class': 'String' }])
 res2 = proxy._in([{ 'class': 'String'} , "distsys", { 'class': 'String'} ])
-res3 = proxy._in([{ 'class': 'String'} , "distsys", { 'class': 'Numeric'} ])
+res3 = proxy._in([{ 'class': 'String'} , "distsys", { 'class': 'String'} ])
 res4 = proxy._in([{ 'class': 'String'} , "distsys", { 'class': 'String'} ])
 
 print("Sent tuples...")
